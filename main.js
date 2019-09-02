d6c7b0425755e435e254a0cd4ca4a4706d8d104d
tapia1013 <53988501+tapia1013@users.noreply.github.com>
2019-09-02 11:21:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+selector(".menu").addEventListener('click', function () {
+  this.classList.toggle('open');
+  selector('header').classList.toggle('open');
+  selector('overlay').classList.toggle('open');
+});
+
+
+
+
+
+function selector(s) {
+  return document.querySelector(s);
+}
